66b40b180141a73a93b1b1e0896090d2d578c4f6
AnthonyTerreros <dterrero@espol.edu.ec>
2024-09-02 23:37:07 -0500

    Feature: Configuracion del proyecto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // src/index.js
-import express from 'express';
+import express from "express";
 import clientesRoutes from "./routers/clientes.routes.js";
 import empleadosRoutes from "./routers/empleados.routes.js";
 import actividadesRoutes from "./routers/actividades.routes.js";
-import cors from 'cors';
-import dotenv from 'dotenv';
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -13,13 +13,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 app.use("/api", clientesRoutes);
 app.use("/api", empleadosRoutes);
 app.use("/api", actividadesRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('listening on port:', PORT);
+  console.log("listening on port:", PORT);
 });
